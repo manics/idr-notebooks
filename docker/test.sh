f53db557374a881eac07cac0afad59d335e737db
Simon Li <spli@dundee.ac.uk>
2019-01-15 11:46:31 +0000

    Update docker/test.sh to run test_notebooks.sh
    Update Travis to run test_notebooks.sh on branches but not PRs

--- docker/test.sh
+++ docker/test.sh
@@ -6,15 +6,26 @@
 # This will exclude notebooks that take a long time to execute or require
 # manual input; you should test these manually in the Jupyter notebook web
 # interface.
-# See the `pytest.mark.xfail` markers in test_notebooks.py for details.
+# See comments in test_notebooks.sh for details.
 
-set -eu
-set -x
+set -e
+
 docker rm -f test-jupyter-docker || true
 docker build -t test-jupyter-docker ..
-docker run -d --name test-jupyter-docker \
+docker run -d -p 8888:8888 --name test-jupyter-docker \
     -e IDR_HOST="$IDR_HOST" \
     -e IDR_USER="$IDR_USER" \
     -e IDR_PASSWORD="$IDR_PASSWORD" \
     test-jupyter-docker
-docker exec test-jupyter-docker /home/jovyan/notebooks/docker/test_notebooks.sh
+
+sleep 10
+curl -fL http://localhost:8888/ | head
+
+# IDR_* variables are only defined on the official branches and when testing
+# locally
+if [ -n "$TRAVIS_PULL_REQUEST" -a "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    echo "WARNING: Skipping test_notebooks.sh (TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST)"
+else
+    echo Running test_notebooks.sh
+    docker exec test-jupyter-docker /home/jovyan/notebooks/docker/test_notebooks.sh
+fi
